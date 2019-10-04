8ce1e198c3c162bd7a34cf321de6628c0639d3a0
Wesley Alkemade <wesleyalkemade@outlook.com>
2019-10-04 23:37:55 +0200

    Beatmapset HTTP service

---
+++ src/app/services/get-beatmap.ts
@@ -0,0 +1,78 @@
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Beatmap } from '../models/beatmap';
+import { Injectable } from '@angular/core';
+import { StoreService } from './store.service';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class GetBeatmap {
+    private url: string = "https://osu.ppy.sh/";
+    private endpoint: string = "api/get_beatmaps";
+    private key: string;
+
+    constructor(private httpClient: HttpClient, private storeService: StoreService) {
+        this.key = storeService.get('api-key');
+    }
+
+    /**
+     * Grab a beatmapset from the osu! api with the given beatmapsetId
+     * @param beatmapsetId the beatmapsetId to grab the data from
+     */
+    public getByBeatmapsetId(beatmapsetId: number): Observable<Beatmap> {
+        return this.httpClient.get<Beatmap>(`${this.url}${this.endpoint}?k=${this.key}&s=${beatmapsetId}`)
+        .pipe(
+            map((data: any) => this.serializeFromJson(data))
+        );
+    }
+
+    serializeFromJson(json: any): Beatmap {
+        const beatmap = new Beatmap();
+
+        // Unwrap json object > osu api gives back [{...}]
+        [json] = json;
+
+        beatmap.approved = json.approved;
+        beatmap.submit_date = json.submit_date;
+        beatmap.approved_date = json.approved_date;
+        beatmap.last_update = json.last_update;
+        beatmap.artist = json.aritst;
+        beatmap.beatmap_id = json.beatmap_id;
+        beatmap.beatmapset_id = json.beatmapset_id;
+        beatmap.bpm = json.bpm;
+        beatmap.creator = json.creator;
+        beatmap.creator_id = json.creator_id;
+        beatmap.difficultyrating = json.difficultyrating;
+        beatmap.diff_aim = json.diff_aim;
+        beatmap.diff_speed = json.diff_speed;
+        beatmap.diff_size = json.diff_size;
+        beatmap.diff_overall = json.diff_overall;
+        beatmap.diff_approach = json.diff_approach;
+        beatmap.diff_drain = json.diff_drain;
+        beatmap.hit_length = json.hit_length;
+        beatmap.source = json.source;
+        beatmap.genre_id = json.genre_id;
+        beatmap.language_id = json.language_id;
+        beatmap.title = json.title;
+        beatmap.total_length = json.total_length;
+        beatmap.version = json.version;
+        beatmap.file_md5 = json.file_md5;
+        beatmap.mode = json.mode;
+        beatmap.tags = json.tags;
+        beatmap.favourite_count = json.favourite_count;
+        beatmap.rating = json.rating;
+        beatmap.playcount = json.playcount;
+        beatmap.passcount = json.passcount;
+        beatmap.count_normal = json.count_normal;
+        beatmap.count_slider = json.count_slider;
+        beatmap.count_spinner = json.count_spinner;
+        beatmap.max_combo = json.max_combo;
+        beatmap.download_unavailable = json.download_unavailable;
+        beatmap.audio_unavailable = json.audio_unavailable;
+
+        return beatmap;
+    }
+}
