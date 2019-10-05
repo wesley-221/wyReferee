ade3e4dfb588f69b55d77f9c04382327f79720e2
Wesley Alkemade <wesleyalkemade@outlook.com>
2019-10-05 17:41:03 +0200

    Add api-key validation

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { ElectronService } from '../../services/electron.service';
 import { StoreService } from '../../services/store.service';
+import { ToastService } from '../../services/toast.service';
+import { ToastType } from '../../models/toast';
+import { ApiKeyValidation } from '../../services/api-key-validation.service';
 
 @Component({
 	selector: 'app-settings',
@@ -11,7 +14,12 @@ import { StoreService } from '../../services/store.service';
 export class SettingsComponent implements OnInit {
 	@ViewChild('apiKey', { static: false }) apiKey: ElementRef;
 
-	constructor(private electronService: ElectronService, private storeService: StoreService) { }
+	constructor(
+		private electronService: ElectronService, 
+		private storeService: StoreService, 
+		private toastService: ToastService,
+		private apiKeyValidation: ApiKeyValidation
+	) { }
 	ngOnInit() { }
 
 	/**
@@ -23,9 +31,25 @@ export class SettingsComponent implements OnInit {
 
 	/**
 	 * Save the api key with the entered value
-	 * TODO: Validation :)
 	 */
 	saveApiKey() {
-		this.storeService.set('api-key', this.apiKey.nativeElement.value);
+		const apiKey = this.storeService.get('api-key');
+
+		// Check if the key hasn't changed
+		if(apiKey == this.apiKey.nativeElement.value) {
+			this.toastService.addToast('The entered api-key is the same as the one already saved.', ToastType.Information);
+		}
+		else {
+			this.storeService.set('api-key', this.apiKey.nativeElement.value);
+
+			// Key is valid
+			this.apiKeyValidation.validate(this.apiKey.nativeElement.value).subscribe(() => {
+				this.toastService.addToast('You have entered a valid api-key.', ToastType.Information);
+			}, 
+			// Key is invalid
+			err => {
+				this.toastService.addToast('The entered api-key was invalid.', ToastType.Error);
+			});
+		}
 	}
 }
---
+++ src/app/services/api-key-validation.service.ts
@@ -0,0 +1,23 @@
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class ApiKeyValidation {
+	// The best player in osu!
+	userId: number = 2407265;
+
+  	constructor(private httpClient: HttpClient) { }
+
+	/**
+	 * Validate an api key by making a call to the osu! api
+	 * @param apiKey the api key to validate
+	 */
+	validate(apiKey: number): Observable<User> {
+		return this.httpClient.get<User>(`https://osu.ppy.sh/api/get_user?k=${apiKey}&u=${this.userId}&type=id`);
+	}
+}
