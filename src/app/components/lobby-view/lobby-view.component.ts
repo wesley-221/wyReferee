99cef45ca96a4e0cacaf59e3073ca3a32c7d5c31
Wesley Alkemade <wesleyalkemade@outlook.com>
2019-10-06 15:33:40 +0200

    Temporary lobby view

--- src/app/components/lobby-view/lobby-view.component.ts
+++ src/app/components/lobby-view/lobby-view.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { MultiplayerLobby } from '../../models/multiplayer-lobby';
 import { MultiplayerLobbiesService } from '../../services/multiplayer-lobbies.service';
+import { ElectronService } from '../../services/electron.service';
 
 @Component({
 	selector: 'app-lobby-view',
@@ -15,11 +16,31 @@ export class LobbyViewComponent implements OnInit {
 	teamOneScore = 8;
 	teamTwoScore = 5;
 
-	constructor(private route: ActivatedRoute, private multiplayerLobbies: MultiplayerLobbiesService) {
+	constructor(private route: ActivatedRoute, private multiplayerLobbies: MultiplayerLobbiesService, private elctronService: ElectronService) {
 		this.route.params.subscribe(params => {
 			this.selectedLobby = multiplayerLobbies.get(params.id);
+
+			console.log(this.selectedLobby)
 		});
 	}
 
 	ngOnInit() { }
+
+	getThumbUrl(thumbName: string) {
+		return `url('https://b.ppy.sh/thumb/${thumbName}.jpg')`;
+	}
+
+	addDot(nStr, splitter) {
+        nStr += '';
+        const x = nStr.split('.');
+        let x1 = x[0];
+        const x2 = x.length > 1 ? '.' + x[1] : '';
+        const rgx = /(\d+)(\d{3})/;
+
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + splitter + '$2');
+        }
+
+        return x1 + x2;
+    }
 }
