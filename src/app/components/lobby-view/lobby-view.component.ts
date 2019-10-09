5b2ef56c1a76571d961216fc18b4e233ad7c9262
Wesley Alkemade <wesleyalkemade@outlook.com>
2019-10-09 17:20:38 +0200

    Add some extra functions to prevent errors from happening

--- src/app/components/lobby-view/lobby-view.component.ts
+++ src/app/components/lobby-view/lobby-view.component.ts
@@ -5,6 +5,8 @@ import { MultiplayerLobbiesService } from '../../services/multiplayer-lobbies.se
 import { ElectronService } from '../../services/electron.service';
 import { ToastService } from '../../services/toast.service';
 import { CacheService } from '../../services/cache.service';
+import { MultiplayerData } from '../../models/multiplayer-data';
+import { MultiplayerDataUser } from '../../models/multiplayer-data-user';
 
 @Component({
 	selector: 'app-lobby-view',
@@ -15,7 +17,12 @@ import { CacheService } from '../../services/cache.service';
 export class LobbyViewComponent implements OnInit {
 	selectedLobby: MultiplayerLobby;
 
-	constructor(private route: ActivatedRoute, private multiplayerLobbies: MultiplayerLobbiesService, private toastService: ToastService, private cacheService: CacheService) {
+	constructor(
+		private route: ActivatedRoute, 
+		private multiplayerLobbies: MultiplayerLobbiesService, 
+		private toastService: ToastService, 
+		private cacheService: CacheService,
+		private electronService: ElectronService) {
 		this.route.params.subscribe(params => {
 			this.selectedLobby = multiplayerLobbies.get(params.id);
 		});
@@ -23,22 +30,71 @@ export class LobbyViewComponent implements OnInit {
 
 	ngOnInit() { }
 
+	/**
+	 * Synchronizes the multiplayer lobby and calculates all the scores
+	 */
 	synchronizeMp() {
 		this.toastService.addToast('Synchronizing multiplayer lobby...');
 
+		// console.time('synchronize-lobby');
 		this.multiplayerLobbies.synchronizeMultiplayerMatch(this.selectedLobby);
+		// console.timeEnd('synchronize-lobby');
 	}
 
-	getThumbUrl(thumbName: string) {
-		return `url('https://b.ppy.sh/thumb/${thumbName}.jpg')`;
+	/**
+	 * Get the cached beatmap if it exists
+	 * @param beatmapId the beatmapid
+	 */
+	getBeatmapname(beatmapId: number) {
+		const cachedBeatmap = this.cacheService.getCachedBeatmap(beatmapId);
+		return (cachedBeatmap != null) ? cachedBeatmap.name : `Unknown beatmap, synchronize the lobby to get the map name.`;
+	}
+	
+	/**
+	 * Get the beatmap image
+	 * @param beatmapId the beatmapid
+	 */
+	getThumbUrl(beatmapId: number) {
+		const cachedBeatmap = this.cacheService.getCachedBeatmap(beatmapId);
+		return (cachedBeatmap != null) ? `url('https://b.ppy.sh/thumb/${cachedBeatmap.beatmapset_id}.jpg')` : ``;
 	}
 
+	/**
+	 * Get the cached username 
+	 * @param userId the userid
+	 */
 	getUsernameFromUserId(userId: number) {
 		const cachedUser = this.cacheService.getCachedUser(userId);
-		
 		return (cachedUser != null) ? cachedUser.username : "Unknown";
 	}
 
+	/**
+	 * Get the accuracy of the player in the given slot
+	 * @param match the MultiplayerData
+	 * @param slotId the slot you want the accuracy from
+	 */
+	getAccuracy(match: MultiplayerData, slotId: number) {
+		const user: MultiplayerDataUser = match.getPlayer(slotId);
+
+		return (user != undefined) ? user.accuracy : 0.00
+	}
+
+	/**
+	 * Get the score of the player in the given slot
+	 * @param match the MultiplayerData
+	 * @param slotId the slot you want the score from
+	 */
+	getScore(match: MultiplayerData, slotId: number) {
+		const user: MultiplayerDataUser = match.getPlayer(slotId);
+
+		return (user != undefined) ? this.addDot(user.score.toFixed(), " ") : 0;
+	}
+
+	/**
+	 * Split the string 
+	 * @param nStr the string to split
+	 * @param splitter the character to split the string with
+	 */
 	addDot(nStr, splitter) {
         nStr += '';
         const x = nStr.split('.');
