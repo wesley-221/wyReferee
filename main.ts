e878a20b537f289578236c30d01fe549c01d4c15
Wesley Alkemade <wesleyalkemade@outlook.com>
2019-10-09 18:00:46 +0200

    Remove unused files and added some small changes

--- main.ts
+++ main.ts
@@ -7,72 +7,77 @@ const args = process.argv.slice(1);
 serve = args.some(val => val === '--serve');
 
 function createWindow() {
+	const electronScreen = screen;
+	const size = electronScreen.getPrimaryDisplay().workAreaSize;
 
-  const electronScreen = screen;
-  const size = electronScreen.getPrimaryDisplay().workAreaSize;
+	// Create the browser window.
+	win = new BrowserWindow({
+		x: 0,
+		y: 0,
+		width: size.width,
+		height: size.height,
+		webPreferences: {
+			nodeIntegration: true,
+		},
+		frame: false,
+		titleBarStyle: 'hidden',
+		icon: `${__dirname}/src/assets/images/axs.ico`
+	});
 
-  // Create the browser window.
-  win = new BrowserWindow({
-    x: 0,
-    y: 0,
-    width: size.width,
-    height: size.height,
-    webPreferences: {
-      nodeIntegration: true,
-    },
-    frame: false
-  });
+	// Check for OS
+	if(process.platform === 'linux') {
+		win.icon = path.join(`${__dirname}/src/assets/images/axs.png`);
+	}
 
-  if (serve) {
-    require('electron-reload')(__dirname, {
-      electron: require(`${__dirname}/node_modules/electron`)
-    });
-    win.loadURL('http://localhost:4200');
-  } else {
-    win.loadURL(url.format({
-      pathname: path.join(__dirname, 'dist/index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
-  }
+	if (serve) {
+		require('electron-reload')(__dirname, {
+			electron: require(`${__dirname}/node_modules/electron`)
+		});
 
-  if (serve) {
-    win.webContents.openDevTools();
-  }
+		win.loadURL('http://localhost:4200');
+	} else {
+		win.loadURL(url.format({
+			pathname: path.join(__dirname, 'dist/index.html'),
+			protocol: 'file:',
+			slashes: true
+		}));
+	}
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store window
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null;
-  });
+	if (serve) {
+		win.webContents.openDevTools();
+	}
 
+	// Emitted when the window is closed.
+	win.on('closed', () => {
+		// Dereference the window object, usually you would store window
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		win = null;
+	});
 }
 
 try {
+	// This method will be called when Electron has finished
+	// initialization and is ready to create browser windows.
+	// Some APIs can only be used after this event occurs.
+	app.on('ready', createWindow);
 
-  // This method will be called when Electron has finished
-  // initialization and is ready to create browser windows.
-  // Some APIs can only be used after this event occurs.
-  app.on('ready', createWindow);
+	// Quit when all windows are closed.
+	app.on('window-all-closed', () => {
+		// On OS X it is common for applications and their menu bar
+		// to stay active until the user quits explicitly with Cmd + Q
+		if (process.platform !== 'darwin') {
+			app.quit();
+		}
+	});
 
-  // Quit when all windows are closed.
-  app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-      app.quit();
-    }
-  });
-
-  app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-      createWindow();
-    }
-  });
+	app.on('activate', () => {
+		// On OS X it's common to re-create a window in the app when the
+		// dock icon is clicked and there are no other windows open.
+		if (win === null) {
+			createWindow();
+		}
+	});
 
 } catch (e) {
   // Catch Error
