c1da10bb63a1d7efb0a56211e4052c84d6ad95b4
Wesley Alkemade <wesleyalkemade@outlook.com>
2019-10-18 14:39:58 +0200

    Api key check and extra styles for toast messages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { ElectronService } from './services/electron.service';
+import { StoreService } from './services/store.service';
+import { ToastService } from './services/toast.service';
+import { ToastType } from './models/toast';
 
 @Component({
 	selector: 'app-root',
@@ -7,5 +10,12 @@ import { ElectronService } from './services/electron.service';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	constructor(public electronService: ElectronService) {}
+	constructor(public electronService: ElectronService, private storeService: StoreService, private toastService: ToastService) {
+		const apiKey = storeService.get('api-key');
+
+		// Check if the api key is set
+		if(apiKey == undefined || apiKey == '') {
+			toastService.addToast(`The api key wasn't set, make sure to set the api key before continuing.`, ToastType.Error, 20);
+		}
+	}
 }
