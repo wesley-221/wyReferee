bcb5d04ec9384fc87371af5e1238995dcd7c6d63
Wesley <wesleyalkemade@outlook.com>
2020-04-11 20:17:20 +0200

    Add tournament selection in the create a lobby screen

--- src/app/components/lobby/create-lobby/create-lobby.component.ts
+++ src/app/components/lobby/create-lobby/create-lobby.component.ts
@@ -6,6 +6,8 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { IrcService } from '../../../services/irc.service';
 import { ScoreInterface } from '../../../models/score-calculation/calculation-types/score-interface';
 import { Calculate } from '../../../models/score-calculation/calculate';
+import { TournamentService } from '../../../services/tournament.service';
+import { Tournament } from '../../../models/tournament/tournament';
 
 @Component({
 	selector: 'app-create-lobby',
@@ -19,6 +21,7 @@ export class CreateLobbyComponent implements OnInit {
 	multiplayerLobby: string;
 	tournamentAcronym: string;
 	matchDescription: string;
+	selectedTournament: Tournament;
 	teamSize: number;
 	selectedScoreInterface: ScoreInterface;
 
@@ -29,7 +32,10 @@ export class CreateLobbyComponent implements OnInit {
 
 	calculateScoreInterfaces: Calculate;
 
-	constructor(private multiplayerLobbies: MultiplayerLobbiesService, private toastService: ToastService, private ircService: IrcService) {
+	teamOneArray: number[] = [];
+	teamTwoArray: number[] = [];
+
+	constructor(private multiplayerLobbies: MultiplayerLobbiesService, private toastService: ToastService, private ircService: IrcService, public tournamentService: TournamentService) {
 		this.calculateScoreInterfaces = new Calculate();
 
 		ircService.getIsAuthenticated().subscribe(isAuthenticated => {
@@ -59,8 +65,20 @@ export class CreateLobbyComponent implements OnInit {
 			'teamTwoName': new FormControl('', [
 				Validators.required
 			]),
-			'webhook': new FormControl()
+			'webhook': new FormControl(),
+			'selectedTournament': new FormControl()
 		});
+	}
+
+	changeTournament() {
+		this.selectedTournament = this.tournamentService.getTournamentByName(this.validationForm.get('selectedTournament').value);
+		this.changeTeamSize(this.selectedTournament != null ? this.selectedTournament.teamSize : null);
+
+		this.selectedScoreInterface = this.calculateScoreInterfaces.getScoreInterface(this.selectedTournament ? this.selectedTournament.tournamentScoreInterfaceIdentifier : null);
+		this.teamSize = this.selectedScoreInterface ? this.selectedScoreInterface.getTeamSize() : null;
+		this.validationForm.get('teamSize').setValue(this.selectedTournament != null ? this.selectedTournament.teamSize : this.teamSize);
+		this.validationForm.get('tournamentAcronym').setValue(this.selectedTournament != null ? this.selectedTournament.acronym : null);
+		this.validationForm.get('scoreInterface').setValue(this.selectedScoreInterface ? this.selectedScoreInterface.getIdentifier() : null);
 	}
 
 	changeScoreInterface(event: Event) {
@@ -81,7 +99,7 @@ export class CreateLobbyComponent implements OnInit {
 		newLobby.tournamentAcronym = this.validationForm.get('tournamentAcronym').value;
 		newLobby.description = `${this.validationForm.get('teamOneName').value} vs ${this.validationForm.get('teamTwoName').value}`;
 		newLobby.webhook = this.validationForm.get('webhook').value;
-		newLobby.scoreInterfaceIndentifier = this.selectedScoreInterface.getIdentifier();
+		newLobby.scoreInterfaceIndentifier = this.selectedTournament ? this.selectedTournament.tournamentScoreInterfaceIdentifier : this.selectedScoreInterface.getIdentifier();
 
 		if(newLobby.multiplayerLink == '') {
 			this.ircService.isCreatingMultiplayerLobby = newLobby.lobbyId;
@@ -102,5 +120,29 @@ export class CreateLobbyComponent implements OnInit {
 
 	getValidation(key: string): any {
 		return this.validationForm.get(key);
+	}
+
+	changeTeamSize(teamSize: number) {
+		this.teamOneArray = [];
+		this.teamTwoArray = [];
+
+		let teamSizeVal;
+
+		if(teamSize == null || teamSize == undefined) {
+			teamSizeVal = this.getValidation('teamSize').value >= 8 ? 8 : this.getValidation('teamSize').value;
+		}
+		else {
+			teamSizeVal = teamSize >= 8 ? 8 : teamSize;
+		}
+
+		teamSizeVal = parseInt(teamSizeVal);
+
+		for(let i = 1; i < (teamSizeVal + 1); i ++) {
+			this.teamOneArray.push(i);
+		}
+
+		for(let i = teamSizeVal + 1; i < ((teamSizeVal * 2) + 1); i ++) {
+			this.teamTwoArray.push(i);
+		}
 	}
 }
