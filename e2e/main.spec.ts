a28fbaa7d2e11b47d314e61ae6b2987de29aabab
Wesley <wesleyalkemade@outlook.com>
2020-08-26 23:30:16 +0200

    update linter

--- e2e/main.spec.ts
+++ e2e/main.spec.ts
@@ -1,28 +1,27 @@
-import {expect, assert} from 'chai';
-import {SpectronClient} from 'spectron';
+import { expect } from 'chai';
+import { SpectronClient } from 'spectron';
 
 import commonSetup from './common-setup';
 
-describe('angular-electron App', function () {
-  commonSetup.apply(this);
+describe('wyReferee App', function () {
+	commonSetup.apply(this);
 
-  let browser: any;
-  let client: SpectronClient;
+	let browser: any;
+	let client: SpectronClient;
 
-  beforeEach(function () {
-    client = this.app.client;
-    browser = client as any;
-  });
+	beforeEach(function () {
+		client = this.app.client;
+		browser = client as any;
+	});
 
-  it('should display message saying App works !', async function () {
-    const text = await browser.getText('app-home h1');
-    expect(text).to.equal('App works !');
-  });
+	it('should display message saying App works !', async function () {
+		const text = await browser.getText('app-home h1');
+		expect(text).to.equal('App works !');
+	});
 
-
-  it('creates initial windows', async function () {
-    const count = await client.getWindowCount();
-    expect(count).to.equal(1);
-  });
+	it('creates initial windows', async function () {
+		const count = await client.getWindowCount();
+		expect(count).to.equal(1);
+	});
 
 });
