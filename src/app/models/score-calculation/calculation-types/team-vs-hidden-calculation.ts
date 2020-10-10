45a0ab8271a2217224bf8ea8ed88c649a322032a
Wesley <wesleyalkemade@outlook.com>
2020-10-10 22:18:14 +0200

    An example score interface with custom score multipliers

---
+++ src/app/models/score-calculation/calculation-types/team-vs-hidden-calculation.ts
@@ -0,0 +1,67 @@
+import { MultiplayerDataUser } from 'app/models/store-multiplayer/multiplayer-data-user';
+import { ScoreInterface } from './score-interface';
+
+/**
+ * Example score interface for making Hidden worth 1.06x again with ScoreV2
+ */
+export class TeamVsHiddenCalculation extends ScoreInterface {
+	constructor(identifier: string) {
+		super(identifier);
+
+		this.setDescription('Team vs. with Hidden 1.06x modifier');
+	}
+
+	calculatePlayerScore(player: MultiplayerDataUser): number {
+		let newScore = player.score;
+
+		if((player.mods == 24) && player.mods != 0) {
+			newScore /= 1.12;
+		}
+
+		if((player.mods == 72) && player.mods != 0) {
+			newScore /= 1.06;
+		}
+
+		// Score is now without any modifiers
+		newScore = Math.ceil(newScore);
+
+		// Just hidden
+		if((player.mods == 8) && player.mods != 0) {
+			newScore *= 1.06;
+		}
+
+		// Hidden + Hardrock
+		if((player.mods == 24) && player.mods != 0) {
+			newScore *= 1.18;
+		}
+
+		// Hidden + Doubletime
+		if((player.mods == 72) && player.mods != 0) {
+			newScore *= 1.12;
+		}
+
+		newScore = Math.ceil(newScore);
+
+		return Number(player != null ? player.passed == 1 ? newScore : 0 : 0);
+	}
+
+	calculateTeamOneScore() {
+		let teamScore = 0;
+
+		for (let i = 0; i < this.getTeamSize(); i++) {
+			teamScore += this.calculatePlayerScore(this.getUserBySlot(i));
+		}
+
+		return teamScore;
+	}
+
+	calculateTeamTwoScore() {
+		let teamScore = 0;
+
+		for (let i = this.getTeamSize(); i < this.getTeamSize() * 2; i++) {
+			teamScore += this.calculatePlayerScore(this.getUserBySlot(i));
+		}
+
+		return teamScore;
+	}
+}
