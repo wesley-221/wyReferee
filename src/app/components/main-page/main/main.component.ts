13d1878c9b10bdb2192ba3c1b3cd3ed2ceb38c0f
Wesley <wesleyalkemade@outlook.com>
2020-10-11 22:09:42 +0200

    Add background for sub-sidebar in management

--- src/app/components/main-page/main/main.component.ts
+++ src/app/components/main-page/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-main',
@@ -7,6 +8,6 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class MainComponent implements OnInit {
-	constructor() { }
+	constructor(public router: Router) { }
 	ngOnInit() { }
 }
