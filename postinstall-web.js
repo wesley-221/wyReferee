27d7b4dba7c659ced8697561e27946143ce644b2
Wesley <wesleyalkemade@outlook.com>
2021-11-24 21:45:48 +0100

    fix(bancho.js): band-aid fix for bancho.js error

--- postinstall-web.js
+++ postinstall-web.js
@@ -14,3 +14,19 @@ fs.readFile(f_angular, 'utf8', function (err, data) {
 		if (err) return console.log(err);
 	});
 });
+
+// Change bancho.js setMap > gamemode type so it doesn't throw an error anymore (hacky solution for now)
+// ERROR in node_modules/bancho.js/index.d.ts:367:55 - error TS2694: Namespace '"nodesu"' has no exported member 'Mode'.
+const banchoJs = 'node_modules/bancho.js/index.d.ts';
+fs.readFile(banchoJs, 'utf8', function (err, data) {
+	if (err) {
+		return console.log(err);
+	}
+
+	const result = data.replace('setMap(map: number|nodesu.Beatmap, gamemode: nodesu.Mode)', 'setMap(map: number|nodesu.Beatmap, gamemode: any)');
+
+	fs.writeFile(banchoJs, result, 'utf8', function (err) {
+		if (err)
+			return console.log(err);
+	});
+});
--- postinstall.js
+++ postinstall.js
@@ -6,11 +6,29 @@ fs.readFile(f_angular, 'utf8', function (err, data) {
 	if (err) {
 		return console.log(err);
 	}
-	var result = data.replace(/target: "electron-renderer",/g, '');
-	var result = result.replace(/target: "web",/g, '');
-	var result = result.replace(/return \{/g, 'return {target: "electron-renderer",');
+
+	let result = data.replace(/target: "electron-renderer",/g, '');
+	let result = result.replace(/target: "web",/g, '');
+	let result = result.replace(/return \{/g, 'return {target: "electron-renderer",');
 
 	fs.writeFile(f_angular, result, 'utf8', function (err) {
-		if (err) return console.log(err);
+		if (err)
+			return console.log(err);
+	});
+});
+
+// Change bancho.js setMap > gamemode type so it doesn't throw an error anymore (hacky solution for now)
+// ERROR in node_modules/bancho.js/index.d.ts:367:55 - error TS2694: Namespace '"nodesu"' has no exported member 'Mode'.
+const banchoJs = 'node_modules/bancho.js/index.d.ts';
+fs.readFile(banchoJs, 'utf8', function (err, data) {
+	if (err) {
+		return console.log(err);
+	}
+
+	const result = data.replace('setMap(map: number|nodesu.Beatmap, gamemode: nodesu.Mode)', 'setMap(map: number|nodesu.Beatmap, gamemode: any)');
+
+	fs.writeFile(banchoJs, result, 'utf8', function (err) {
+		if (err)
+			return console.log(err);
 	});
 });
