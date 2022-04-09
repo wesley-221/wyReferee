271c175fd8b76491a7d49fdde15b6eddfd096a05
Wesley <wesleyalkemade@outlook.com>
2022-04-09 18:25:32 +0200

    feat(irc): press enter to join channel

--- src/app/components/dialogs/join-irc-channel/join-irc-channel.component.ts
+++ src/app/components/dialogs/join-irc-channel/join-irc-channel.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
@@ -10,10 +10,15 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class JoinIrcChannelComponent implements OnInit {
 	validationForm: FormGroup;
 
-	constructor(@Inject(MAT_DIALOG_DATA) public data: string) {
+	constructor(@Inject(MAT_DIALOG_DATA) public data: string, private dialogRef: MatDialogRef<JoinIrcChannelComponent>) {
 		this.validationForm = new FormGroup({
 			'channel-name': new FormControl('', Validators.required)
 		});
+	}
+
+	@HostListener('window:keyup.Enter', ['$event'])
+	onDialogClick(): void {
+		this.dialogRef.close(this.getChannelName());
 	}
 
 	ngOnInit(): void { }
