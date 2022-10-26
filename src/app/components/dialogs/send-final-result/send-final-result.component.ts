b86871e0d9ef73ec3ede5815b6cccf5db357f038
Wesley <15431544+wesley-221@users.noreply.github.com>
2022-10-27 01:24:03 +0200

    feat(result): implement score updating on wyBin tournament

--- src/app/components/dialogs/send-final-result/send-final-result.component.ts
+++ src/app/components/dialogs/send-final-result/send-final-result.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { MatButtonToggleChange } from '@angular/material/button-toggle';
 import { IMultiplayerLobbySendFinalMessageDialogData } from 'app/interfaces/i-multiplayer-lobby-send-final-message-dialog-data';
+import { LobbyViewComponent } from 'app/modules/lobby/components/lobby-view/lobby-view.component';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { AppConfig } from 'environments/environment';
+import { ToastService } from 'app/services/toast.service';
 
 @Component({
 	selector: 'app-send-final-result',
@@ -10,6 +14,8 @@ import { IMultiplayerLobbySendFinalMessageDialogData } from 'app/interfaces/i-mu
 	styleUrls: ['./send-final-result.component.scss']
 })
 export class SendFinalResultComponent implements OnInit {
+	private readonly apiUrl = AppConfig.apiUrl;
+
 	firstStepFormGroup: FormGroup;
 	secondStepFormGroup: FormGroup;
 
@@ -17,7 +23,9 @@ export class SendFinalResultComponent implements OnInit {
 
 	canSend = false;
 
-	constructor(@Inject(MAT_DIALOG_DATA) public data: IMultiplayerLobbySendFinalMessageDialogData) {
+	loading = false;
+
+	constructor(@Inject(MAT_DIALOG_DATA) public data: IMultiplayerLobbySendFinalMessageDialogData, private dialogRef: MatDialogRef<LobbyViewComponent>, private http: HttpClient, private toastService: ToastService) {
 		this.firstStepFormGroup = new FormGroup({
 			'match-outcome': new FormControl('', Validators.required),
 			'extra-message': new FormControl()
@@ -43,7 +51,7 @@ export class SendFinalResultComponent implements OnInit {
 		this.secondStepFormGroup.markAllAsTouched();
 	}
 
-	returnData(): IMultiplayerLobbySendFinalMessageDialogData {
+	returnData() {
 		let winningTeam = null;
 		let losingTeam = null;
 
@@ -56,13 +64,40 @@ export class SendFinalResultComponent implements OnInit {
 			losingTeam = this.data.multiplayerLobby.getTeamOneScore() > this.data.multiplayerLobby.getTeamTwoScore() ? this.data.multiplayerLobby.teamTwoName : this.data.multiplayerLobby.teamOneName;
 		}
 
-		return {
+		const wyBinTournamentId = this.data.multiplayerLobby.tournament.wyBinTournamentId;
+
+		if (wyBinTournamentId != null && wyBinTournamentId != undefined) {
+			this.loading = true;
+
+			this.http.post<any>(`${this.apiUrl}tournament-wyreferee-score-update`, {
+				tournamentId: wyBinTournamentId,
+				stageName: this.data.multiplayerLobby.selectedStage.name,
+				multiplayerLink: this.data.multiplayerLobby.multiplayerLink,
+				opponentOne: this.data.multiplayerLobby.teamOneName,
+				opponentTwo: this.data.multiplayerLobby.teamTwoName,
+				opponentOneScore: this.data.multiplayerLobby.getTeamOneScore(),
+				opponentTwoScore: this.data.multiplayerLobby.getTeamTwoScore(),
+				winByDefaultWinner: this.secondStepFormGroup.get('winning-team').value
+			}).subscribe(() => {
+				this.closeDialog(winningTeam, losingTeam);
+			}, (err: HttpErrorResponse) => {
+				this.toastService.addToast(err.error.message);
+				this.closeDialog(winningTeam, losingTeam);
+			});
+		}
+		else {
+			this.closeDialog(winningTeam, losingTeam);
+		}
+	}
+
+	closeDialog(winningTeam: string, losingTeam: string): void {
+		this.dialogRef.close({
 			winningTeam: winningTeam,
 			losingTeam: losingTeam,
 			winByDefault: this.isWinByDefault,
 			multiplayerLobby: this.data.multiplayerLobby,
 			extraMessage: this.firstStepFormGroup.get('extra-message').value,
 			qualifierLobby: this.firstStepFormGroup.get('match-outcome').value == 'qualifier-result' ? true : false
-		};
+		});
 	}
 }
