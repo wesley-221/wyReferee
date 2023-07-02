4a861f2827ffee807aee2f234bf640be2e64f935
Wesley <wesleyalkemade@outlook.com>
2023-07-02 22:24:34 +0200

    fix(ctm-score): fix easy bonus score

--- src/app/models/score-calculation/calculation-types/ctm-calculation.ts
+++ src/app/models/score-calculation/calculation-types/ctm-calculation.ts
@@ -5,8 +5,7 @@ import { Mods } from 'app/models/osu-models/osu';
 export class CTMCalculation extends ScoreInterface {
 	private readonly MAX_BONUS_SCORE = 150000;
 
-	private readonly EASY_ONLY_BONUS_SCORE = 500000;
-	private readonly EASY_BONUS_SCORE = 25000;
+	private readonly EASY_BONUS_SCORE = 525000;
 	private readonly HARDROCK_BONUS_SCORE = 50000;
 	private readonly DOUBLETIME_BONUS_SCORE = 75000;
 	private readonly FLASHLIGHT_BONUS_SCORE = 100000;
@@ -14,6 +13,9 @@ export class CTMCalculation extends ScoreInterface {
 
 	private teamOneBonusScore: number;
 	private teamTwoBonusScore: number;
+
+	private teamOneUsedEasy: boolean;
+	private teamTwoUsedEasy: boolean;
 
 	constructor(identifier: string, teamSize: number) {
 		super(identifier);
@@ -23,18 +25,21 @@ export class CTMCalculation extends ScoreInterface {
 
 		this.teamOneBonusScore = 0;
 		this.teamTwoBonusScore = 0;
+
+		this.teamOneUsedEasy = false;
+		this.teamTwoUsedEasy = false;
 	}
 
 	calculatePlayerScore(player: MultiplayerDataUser, team: number): number {
-		let easyOnlyWasUsed = false;
-
-		if ((player.mods & Mods.Easy) == player.mods || (player.mods & (Mods.Easy | Mods.NoFail)) == player.mods) {
+		if (player.mods & Mods.Easy) {
 			this.addTeamBonusScore(this.EASY_BONUS_SCORE, team);
-			easyOnlyWasUsed = true;
-		}
 
-		if (player.mods & Mods.Easy && easyOnlyWasUsed == false) {
-			this.addTeamBonusScore(this.EASY_BONUS_SCORE, team);
+			if (team == 1) {
+				this.teamOneUsedEasy = true;
+			}
+			else if (team == 2) {
+				this.teamTwoUsedEasy = true;
+			}
 		}
 
 		if (player.mods & Mods.HardRock) {
@@ -51,10 +56,6 @@ export class CTMCalculation extends ScoreInterface {
 
 		if (player.mods & Mods.SuddenDeath) {
 			this.addTeamBonusScore(this.SUDDEN_DEATH_BONUS_SCORE, team);
-		}
-
-		if (easyOnlyWasUsed == true) {
-			player.score += this.EASY_ONLY_BONUS_SCORE;
 		}
 
 		return Number(player != null ? player.passed == 1 ? player.score : 0 : 0);
@@ -105,6 +106,7 @@ export class CTMCalculation extends ScoreInterface {
 	 * @param team the team to get the bonus score from
 	 */
 	private getBonusScore(team: number) {
-		return Math.min(team == 1 ? this.teamOneBonusScore : this.teamTwoBonusScore, this.MAX_BONUS_SCORE);
+		const usedEasy = team == 1 ? this.teamOneUsedEasy : this.teamTwoUsedEasy;
+		return usedEasy ? this.EASY_BONUS_SCORE : Math.min(team == 1 ? this.teamOneBonusScore : this.teamTwoBonusScore, this.MAX_BONUS_SCORE);
 	}
 }
