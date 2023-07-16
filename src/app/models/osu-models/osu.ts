8e351b9bf40b56b403d11fc37c10b4fc0bf07512
Wesley <wesleyalkemade@outlook.com>
2023-07-16 19:18:56 +0200

    fix(mod): add nomod to mod abbreviations

--- src/app/models/osu-models/osu.ts
+++ src/app/models/osu-models/osu.ts
@@ -140,7 +140,10 @@ export class OsuHelper {
 			}
 		}
 
-		if (mod.trim().toLowerCase() == 'mixed mod') {
+		if (mod.trim().toLowerCase() == 'nomod') {
+			modAbbreviations = 'nm';
+		}
+		else if (mod.trim().toLowerCase() == 'mixed mod') {
 			modAbbreviations = 'mm';
 		}
 		else if (mod.trim().toLowerCase() == 'tiebreaker') {
