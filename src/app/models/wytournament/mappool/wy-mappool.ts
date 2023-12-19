0d69855699b4f590f0071ab8a8ac7a1e440e950c
Wesley <wesleyalkemade@outlook.com>
2023-12-19 14:46:57 +0100

    fix(mappool): fix variable name to not match upper scope

--- src/app/models/wytournament/mappool/wy-mappool.ts
+++ src/app/models/wytournament/mappool/wy-mappool.ts
@@ -178,14 +178,14 @@ export class WyMappool {
 			}
 
 			for (const beatmap of modBracket.beatmaps) {
-				const gamemodeId = beatmap.gamemodeId == null || beatmap.gamemodeId == undefined ? null : beatmap.gamemodeId;
+				const beatmapGamemodeId = beatmap.gamemodeId == null || beatmap.gamemodeId == undefined ? null : beatmap.gamemodeId;
 
 				const newBeatmap = new WyModBracketMap({
 					beatmapId: beatmap.beatmapId,
 					beatmapName: beatmap.name,
 					beatmapsetId: beatmap.beatmapsetId,
 					beatmapUrl: 'https://osu.ppy.sh/beatmaps/' + beatmap.beatmapId,
-					gamemodeId: gamemodeId,
+					gamemodeId: beatmapGamemodeId,
 					index: newModBracket.beatmapIndex
 				});
 
