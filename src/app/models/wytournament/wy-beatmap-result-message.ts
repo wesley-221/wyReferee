5becd67b8e8878c1100250887fa3353ad62cac81
Wesley <wesleyalkemade@outlook.com>
2024-09-05 17:29:30 +0200

    feat(messages): add 2 new message conditions and redesign layout

--- src/app/models/wytournament/wy-beatmap-result-message.ts
+++ src/app/models/wytournament/wy-beatmap-result-message.ts
@@ -2,11 +2,15 @@ export class WyBeatmapResultMessage {
 	id: number;
 	index: number;
 	message: string;
+	beatmapEnded: boolean;
+	beatmapPicked: boolean;
 	nextPickMessage: boolean;
 	nextPickTiebreakerMessage: boolean;
 	matchWonMessage: boolean;
 
 	constructor(init?: Partial<WyBeatmapResultMessage>) {
+		this.beatmapEnded = false;
+		this.beatmapPicked = false;
 		this.nextPickMessage = false;
 		this.nextPickTiebreakerMessage = false;
 		this.matchWonMessage = false;
@@ -19,6 +23,8 @@ export class WyBeatmapResultMessage {
 			id: beatmapResultMessage.id,
 			index: beatmapResultMessage.index,
 			message: beatmapResultMessage.message,
+			beatmapEnded: beatmapResultMessage.beatmapEnded,
+			beatmapPicked: beatmapResultMessage.beatmapPicked,
 			nextPickMessage: beatmapResultMessage.nextPickMessage,
 			nextPickTiebreakerMessage: beatmapResultMessage.nextPickTiebreakerMessage,
 			matchWonMessage: beatmapResultMessage.matchWonMessage
