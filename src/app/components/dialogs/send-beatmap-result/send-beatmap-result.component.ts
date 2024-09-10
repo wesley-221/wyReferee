efe1698adaeff12e041b8a0dc4fc7d25ddcbf7d9
Wesley <wesleyalkemade@outlook.com>
2024-09-10 14:32:19 +0200

    feat(beatmap-result): make the message translation a function

--- src/app/components/dialogs/send-beatmap-result/send-beatmap-result.component.ts
+++ src/app/components/dialogs/send-beatmap-result/send-beatmap-result.component.ts
@@ -1,8 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ISendBeatmapResultDialogData } from 'app/interfaces/i-send-beatmap-result-dialog-data';
-import { CTMCalculation } from 'app/models/score-calculation/calculation-types/ctm-calculation';
 import { MultiplayerData } from 'app/models/store-multiplayer/multiplayer-data';
+import { WyBeatmapResultMessage } from 'app/models/wytournament/wy-beatmap-result-message';
 import { CacheService } from 'app/services/cache.service';
 import { IrcService } from 'app/services/irc.service';
 
@@ -44,40 +44,8 @@ export class SendBeatmapResultComponent implements OnInit {
 	sendBeatmapResult(match: MultiplayerData) {
 		// User is connected to irc channel
 		if (this.ircService.getChannelByName(this.data.ircChannel) != null) {
-			const replaceWords = {
-				'{{\\s{0,}beatmapWinner\\s{0,}}}': match.team_one_score > match.team_two_score ? this.data.multiplayerLobby.teamOneName : this.data.multiplayerLobby.teamTwoName,
-				'{{\\s{0,}beatmap\\s{0,}}}': `[https://osu.ppy.sh/beatmaps/${match.beatmap_id} ${this.getBeatmapname(match.beatmap_id)}]`,
-				'{{\\s{0,}beatmapTeamOneScore\\s{0,}}}': this.addDot(match.team_one_score, ' '),
-				'{{\\s{0,}beatmapTeamTwoScore\\s{0,}}}': this.addDot(match.team_two_score, ' '),
-				'{{\\s{0,}scoreDifference\\s{0,}}}': match.team_one_score > match.team_two_score ? this.addDot(match.team_one_score - match.team_two_score, ' ') : this.addDot(match.team_two_score - match.team_one_score, ' '),
-				'{{\\s{0,}teamOneName\\s{0,}}}': this.data.multiplayerLobby.teamOneName,
-				'{{\\s{0,}teamTwoName\\s{0,}}}': this.data.multiplayerLobby.teamTwoName,
-				'{{\\s{0,}matchTeamOneScore\\s{0,}}}': this.data.multiplayerLobby.getTeamOneScore(),
-				'{{\\s{0,}matchTeamTwoScore\\s{0,}}}': this.data.multiplayerLobby.getTeamTwoScore(),
-				'{{\\s{0,}nextPick\\s{0,}}}': this.data.multiplayerLobby.getNextPick(),
-				'{{\\s{0,}matchWinner\\s{0,}}}': this.data.multiplayerLobby.teamHasWon()
-			};
-
-			if (this.data.multiplayerLobby.tournament.scoreInterface instanceof CTMCalculation) {
-				replaceWords['{{\\s{0,}teamOneHitpoints\\s{0,}}}'] = this.data.multiplayerLobby.teamOneHealth;
-				replaceWords['{{\\s{0,}teamTwoHitpoints\\s{0,}}}'] = this.data.multiplayerLobby.teamTwoHealth;
-
-				for (const modBracket of this.data.multiplayerLobby.mappool.modBrackets) {
-					for (const beatmap of modBracket.beatmaps) {
-						if (beatmap.beatmapId == match.beatmap_id) {
-							replaceWords['{{\\s{0,}damageDealt\\s{0,}}}'] = beatmap.damageAmount;
-							break;
-						}
-					}
-				}
-			}
-
 			for (const beatmapResultMessage of this.data.multiplayerLobby.tournament.beatmapResultMessages) {
-				let finalMessage = beatmapResultMessage.message;
-
-				for (const regex in replaceWords) {
-					finalMessage = finalMessage.replace(new RegExp(regex), replaceWords[regex]);
-				}
+				let finalMessage = WyBeatmapResultMessage.translateMessage(beatmapResultMessage.message, match, this.data.multiplayerLobby, this.getBeatmapname(match.beatmap_id));
 
 				if (beatmapResultMessage.beatmapResult) {
 					if (beatmapResultMessage.nextPickMessage) {
@@ -103,25 +71,5 @@ export class SendBeatmapResultComponent implements OnInit {
 
 			this.dialogRef.close(true);
 		}
-	}
-
-	/**
-	 * Split the string
-	 *
-	 * @param nStr the string to split
-	 * @param splitter the character to split the string with
-	 */
-	addDot(nStr: string | number, splitter: string) {
-		nStr = nStr.toString();
-		const x = nStr.split('.');
-		let x1: string = x[0];
-		const x2 = x.length > 1 ? `.${x[1]}` : '';
-		const rgx = /(\d+)(\d{3})/;
-
-		while (rgx.test(x1)) {
-			x1 = x1.replace(rgx, `$1${splitter}$2`);
-		}
-
-		return x1 + x2;
 	}
 }
--- src/app/models/wytournament/wy-beatmap-result-message.ts
+++ src/app/models/wytournament/wy-beatmap-result-message.ts
@@ -1,3 +1,8 @@
+import { Misc } from "app/shared/misc";
+import { Lobby } from "../lobby";
+import { CTMCalculation } from "../score-calculation/calculation-types/ctm-calculation";
+import { MultiplayerData } from "../store-multiplayer/multiplayer-data";
+
 export class WyBeatmapResultMessage {
 	id: number;
 	index: number;
@@ -29,5 +34,51 @@ export class WyBeatmapResultMessage {
 			nextPickTiebreakerMessage: beatmapResultMessage.nextPickTiebreakerMessage,
 			matchWonMessage: beatmapResultMessage.matchWonMessage
 		});
+	}
+
+	/**
+	 * Translate the message so that the variables are replaced with all the match information
+	 *
+	 * @param message the message that contains variables to translate
+	 * @param match the match information
+	 * @param multiplayerLobby the multiplayer lobby information
+	 * @param beatmapName the name of the beatmap that was played or picked
+	 */
+	public static translateMessage(message: string, match: MultiplayerData, multiplayerLobby: Lobby, beatmapName: string): string {
+		const replaceWords = {
+			'{{\\s{0,}beatmapWinner\\s{0,}}}': match.team_one_score > match.team_two_score ? multiplayerLobby.teamOneName : multiplayerLobby.teamTwoName,
+			'{{\\s{0,}beatmap\\s{0,}}}': `[https://osu.ppy.sh/beatmaps/${match.beatmap_id} ${beatmapName}]`,
+			'{{\\s{0,}beatmapTeamOneScore\\s{0,}}}': Misc.addDot(match.team_one_score, ' '),
+			'{{\\s{0,}beatmapTeamTwoScore\\s{0,}}}': Misc.addDot(match.team_two_score, ' '),
+			'{{\\s{0,}scoreDifference\\s{0,}}}': match.team_one_score > match.team_two_score ? Misc.addDot(match.team_one_score - match.team_two_score, ' ') : Misc.addDot(match.team_two_score - match.team_one_score, ' '),
+			'{{\\s{0,}teamOneName\\s{0,}}}': multiplayerLobby.teamOneName,
+			'{{\\s{0,}teamTwoName\\s{0,}}}': multiplayerLobby.teamTwoName,
+			'{{\\s{0,}matchTeamOneScore\\s{0,}}}': multiplayerLobby.getTeamOneScore(),
+			'{{\\s{0,}matchTeamTwoScore\\s{0,}}}': multiplayerLobby.getTeamTwoScore(),
+			'{{\\s{0,}nextPick\\s{0,}}}': multiplayerLobby.getNextPick(),
+			'{{\\s{0,}matchWinner\\s{0,}}}': multiplayerLobby.teamHasWon()
+		};
+
+		if (multiplayerLobby.tournament.scoreInterface instanceof CTMCalculation) {
+			replaceWords['{{\\s{0,}teamOneHitpoints\\s{0,}}}'] = multiplayerLobby.teamOneHealth;
+			replaceWords['{{\\s{0,}teamTwoHitpoints\\s{0,}}}'] = multiplayerLobby.teamTwoHealth;
+
+			for (const modBracket of multiplayerLobby.mappool.modBrackets) {
+				for (const beatmap of modBracket.beatmaps) {
+					if (beatmap.beatmapId == match.beatmap_id) {
+						replaceWords['{{\\s{0,}damageDealt\\s{0,}}}'] = beatmap.damageAmount;
+						break;
+					}
+				}
+			}
+		}
+
+		let finalMessage = message;
+
+		for (const regex in replaceWords) {
+			finalMessage = finalMessage.replace(new RegExp(regex), replaceWords[regex]);
+		}
+
+		return finalMessage;
 	}
 }
