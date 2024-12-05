0641e4ff0948b754714eef15cc3a209a123f7b92
Wesley <wesleyalkemade@outlook.com>
2024-12-05 13:59:14 +0100

    fix(accuracy): fixed accuracy so it calculated properly for taiko

--- src/app/models/calculations.ts
+++ src/app/models/calculations.ts
@@ -17,6 +17,9 @@ export class Calculations {
 				accuracy = Number((((Number(score.countgeki) * 305) + (Number(score.count300) * 300) + (Number(score.countkatu) * 200) + (Number(score.count100) * 100) + (Number(score.count50) * 50)) /
 					(305 * (Number(score.countgeki) + Number(score.count300) + Number(score.countkatu) + Number(score.count100) + Number(score.count50) + Number(score.countmiss))) * 100).toFixed(2));
 				break;
+			case Gamemodes.Taiko:
+				accuracy = Number(((Number(score.count300) + (Number(score.count100) * 0.5)) / (Number(score.count300) + Number(score.count100) + Number(score.countmiss)) * 100).toFixed(2));
+				break;
 			default:
 				accuracy = Number(((Number(score.count50) + Number(score.count100) + Number(score.count300)) / (Number(score.count50) + Number(score.count100) + Number(score.count300) + Number(score.countmiss) + Number(score.countkatu)) * 100).toFixed(2));
 				break;
