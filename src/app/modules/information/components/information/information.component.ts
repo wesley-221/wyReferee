8780b7f01d830c6de557b9e9b1f9e8529f5b1862
Wesley <wesleyalkemade@outlook.com>
2025-01-11 19:47:18 +0100

    feat(information): add Discord link to information

--- src/app/modules/information/components/information/information.component.ts
+++ src/app/modules/information/components/information/information.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ElectronService } from 'app/services/electron.service';
+import { AppConfig } from 'environments/environment';
 
 @Component({
 	selector: 'app-information',
@@ -7,6 +8,9 @@ import { ElectronService } from 'app/services/electron.service';
 	styleUrls: ['./information.component.scss']
 })
 export class InformationComponent implements OnInit {
+	githubIssuesLink = AppConfig.links.githubIssues;
+	discordServerLink = AppConfig.links.discordServer;
+
 	constructor(public electronService: ElectronService) { }
 	ngOnInit() { }
 }
