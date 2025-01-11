68316bdb680fac45fad919b34a469da8456dbe26
Wesley <wesleyalkemade@outlook.com>
2025-01-11 19:09:04 +0100

    feat(tournament): hide All tournaments button when not logged in

--- src/app/modules/tournament-management/components/management-router/management-router.component.ts
+++ src/app/modules/tournament-management/components/management-router/management-router.component.ts
@@ -11,7 +11,7 @@ export class ManagementRouterComponent implements OnInit {
 	sidebarMenu: SidebarItem[] = [
 		new SidebarItem({ name: 'Tournament', svgIcon: 'trophy', link: '/tournament-management/tournament-overview' }),
 		new SidebarItem({ name: 'Create', logo: 'add', link: '/tournament-management/tournament-overview/tournament-create', subMenuItem: true }),
-		new SidebarItem({ name: 'All tournaments', htmlElementId: 'tutorial-side-bar-all-tournaments', logo: 'cloud_upload', link: '/tournament-management/tournament-overview/tournament-all-published', subMenuItem: true }),
+		new SidebarItem({ name: 'All tournaments', htmlElementId: 'tutorial-side-bar-all-tournaments', logo: 'cloud_upload', link: '/tournament-management/tournament-overview/tournament-all-published', subMenuItem: true, onlyShowWhenLoggedIn: true }),
 		new SidebarItem({ name: 'Administrator', logo: 'admin_panel_settings', link: '/tournament-management/tournament-overview/tournament-all-published-administrator', subMenuItem: true, onlyShowAsAdministrator: true })
 	];
 
