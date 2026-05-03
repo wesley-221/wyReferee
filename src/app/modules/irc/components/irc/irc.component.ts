22692ca18acb573087787daf9e205033393467aa
Wesley <wesleyalkemade@outlook.com>
2026-05-03 23:10:33 +0200

    feat: move irc lobbies to its own component

--- src/app/modules/irc/components/irc/irc.component.ts
+++ src/app/modules/irc/components/irc/irc.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef, HostListener, OnDestroy } from '@angular/core';
 import { IrcService } from '../../../../services/irc.service';
 import { ElectronService } from '../../../../services/electron.service';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 import { Router } from '@angular/router';
 import { ToastService } from '../../../../services/toast.service';
 import { ToastType } from '../../../../models/toast';
@@ -358,21 +357,6 @@ export class IrcComponent implements OnInit, OnDestroy {
 	}
 
 	/**
-	 * Part from a channel
-	 *
-	 * @param channelName the channel to part
-	 */
-	partChannel(channelName: string) {
-		this.ircService.partChannel(channelName);
-
-		if (this.selectedChannel != undefined && (this.selectedChannel.name == channelName)) {
-			this.selectedChannel = undefined;
-
-			this.normalChats = [];
-		}
-	}
-
-	/**
 	 * Send the entered message to the selected channel
 	 */
 	sendMessage(event: KeyboardEvent) {
@@ -445,17 +429,6 @@ export class IrcComponent implements OnInit, OnDestroy {
 	selectSlashCommand(slashCommand: SlashCommand) {
 		this.chatMessage.nativeElement.value = `/${slashCommand.name}`;
 		this.chatMessage.nativeElement.focus();
-	}
-
-	/**
-	 * Drop a channel to rearrange it
-	 *
-	 * @param event
-	 */
-	dropChannel(event: CdkDragDrop<IrcChannel[]>) {
-		moveItemInArray(this.channels, event.previousIndex, event.currentIndex);
-
-		console.warn('Rearring IRC channels is currently not persisted. Will be added in a future update.');
 	}
 
 	/**
@@ -748,53 +721,6 @@ export class IrcComponent implements OnInit, OnDestroy {
 			this.matchpoint = multiplayerLobby.getMatchPoint();
 			this.tiebreaker = multiplayerLobby.getTiebreaker();
 			this.hasWon = multiplayerLobby.teamHasWon();
-		}
-	}
-
-	/**
-	 * Play a sound when a message is being send to a specific channel
-	 *
-	 * @param channel the channel that should where a message should be send from
-	 * @param status mute or unmute the sound
-	 */
-	playSound(channel: IrcChannel, status: boolean) {
-		channel.playSoundOnMessage = status;
-		window.electronApi.irc.setIrcPlaySoundOnMessage(channel.name, status);
-
-		this.toastService.addToast(`${channel.name} will ${status == false ? 'no longer beep on message' : 'now beep on message'}.`);
-	}
-
-	/**
-	 * Edit the label of a channel
-	 *
-	 * @param channel the channel to edit the label for
-	 */
-	editLabel(channel: IrcChannel): void {
-		channel.editingLabel = !channel.editingLabel;
-
-		// Stopped editing the label
-		if (channel.editingLabel == false) {
-			window.electronApi.irc.setIrcChannelLabel(channel.name, channel.label);
-		}
-		else {
-			// Store old label when starting to edit so we can revert if canceled
-			channel.oldLabel = channel.label;
-		}
-	}
-
-	/**
-	 * Cancel editing the label of a channel
-	 *
-	 * @param channel the channel to cancel editing the label for
-	 */
-	cancelEditLabel(channel: IrcChannel): void {
-		channel.editingLabel = !channel.editingLabel;
-
-		// When creating label for the first time channel.oldLabel will get set to undefined since channel.label will be undefined
-		if (channel.oldLabel !== undefined && channel.oldLabel !== null) {
-			channel.label = channel.oldLabel;
-
-			window.electronApi.irc.setIrcChannelLabel(channel.name, channel.label);
 		}
 	}
 
--- src/app/modules/irc/irc.module.ts
+++ src/app/modules/irc/irc.module.ts
@@ -6,13 +6,15 @@ import { IrcComponent } from 'app/modules/irc/components/irc/irc.component';
 import { SharedModule } from 'app/shared/shared.module';
 import { IrcShortcutCommandsComponent } from './components/irc-shortcut-commands/irc-shortcut-commands.component';
 import { IrcPlayerManagementComponent } from './components/irc-player-management/irc-player-management.component';
+import { IrcLobbiesComponent } from './components/irc-lobbies/irc-lobbies.component';
 
 
 @NgModule({
 	declarations: [
 		IrcComponent,
 		IrcShortcutCommandsComponent,
-		IrcPlayerManagementComponent
+		IrcPlayerManagementComponent,
+		IrcLobbiesComponent
 	],
 	imports: [
 		CommonModule,
