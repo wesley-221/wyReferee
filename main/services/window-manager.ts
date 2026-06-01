a8b27dc480d81c57667ac7394d7e6066cc05d718
Wesley <wesleyalkemade@outlook.com>
2026-06-01 16:46:58 +0200

    fix: change zoom in/out logic

--- main/services/window-manager.ts
+++ main/services/window-manager.ts
@@ -4,6 +4,8 @@ import * as path from "path";
 
 export class WindowManager {
 	win: BrowserWindow | null = null;
+	ZOOM_MIN = -5;
+	ZOOM_MAX = 5;
 
 	constructor(private serve: boolean) { }
 
@@ -44,6 +46,50 @@ export class WindowManager {
 			this.win.loadURL(url);
 		}
 
+		this.win.webContents.on('before-input-event', (event, input) => {
+			if (input.type !== 'keyDown') {
+				return;
+			}
+
+			if (!input.control && !input.meta) {
+				return;
+			}
+
+			switch (input.code) {
+				case 'Equal':
+					this.applyZoom(this.win, 0.5);
+					break;
+
+				case 'Minus':
+					this.applyZoom(this.win, -0.5);
+					break;
+
+				case 'Digit0':
+					this.applyZoom(this.win, 0);
+					break;
+				default:
+					return;
+			}
+
+			event.preventDefault();
+		});
+
 		return this.win;
+	}
+
+	private applyZoom(window: BrowserWindow | null, zoomLevel: number) {
+		if (window == null) {
+			return;
+		}
+
+		const win = window?.webContents;
+
+		if (zoomLevel === 0) {
+			win?.setZoomLevel(0);
+		}
+		else {
+			const next = win?.zoomLevel + zoomLevel;
+			win?.setZoomLevel(Math.min(this.ZOOM_MAX, Math.max(this.ZOOM_MIN, next)));
+		}
 	}
 }
