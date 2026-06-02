313412881f156bcfa1884dc576da3afa519503bf
Wesley <wesleyalkemade@outlook.com>
2026-06-02 16:47:26 +0200

    refactor: update match dialog logic to be lobby specific

--- src/app/modules/irc/components/irc-match-result/irc-match-result.component.ts
+++ src/app/modules/irc/components/irc-match-result/irc-match-result.component.ts
@@ -1,16 +1,17 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { Lobby } from '../../../../models/lobby';
 import { MultiplayerData } from '../../../../models/store-multiplayer/multiplayer-data';
 import { CacheService } from '../../../../services/cache.service';
 import { IrcService } from '../../../../services/irc.service';
-import { combineLatest, map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { MatchDialogDataContextService } from '../../../../services/match-dialog-data-context.service';
 
 @Component({
 	selector: 'app-irc-match-result',
 	templateUrl: './irc-match-result.component.html',
 	styleUrl: './irc-match-result.component.scss'
 })
-export class IrcMatchResultComponent {
+export class IrcMatchResultComponent implements OnChanges {
 	@Input() selectedLobby: Lobby;
 
 	@Output() closeMatchDialogEmitter = new EventEmitter<void>();
@@ -19,22 +20,19 @@ export class IrcMatchResultComponent {
 
 	hasWon$ = this.ircService.hasWon$;
 
-	matchDialogData$ = combineLatest([
-		this.ircService.matchDialogHeaderName$,
-		this.ircService.matchDialogMultiplayerData$,
-		this.ircService.matchDialogSendFinalResult$
-	]).pipe(
-		map(([headerName, multiplayerData, sendFinalResult]) => ({
-			headerName,
-			multiplayerData,
-			sendFinalResult
-		}))
-	);
+	matchDialogData$: Observable<{ headerName: string; multiplayerData: MultiplayerData; sendFinalResult: boolean; }>;
 
 	constructor(
 		private cacheService: CacheService,
-		private ircService: IrcService
+		private ircService: IrcService,
+		private matchDialogDataContextService: MatchDialogDataContextService
 	) { }
+
+	ngOnChanges(changes: SimpleChanges): void {
+		if (changes['selectedLobby']?.currentValue) {
+			this.matchDialogData$ = this.matchDialogDataContextService.getMatchDialogData(this.selectedLobby.lobbyId);
+		}
+	}
 
 	getBeatmapName(beatmapId: number): string {
 		return this.cacheService.getBeatmapname(beatmapId);
