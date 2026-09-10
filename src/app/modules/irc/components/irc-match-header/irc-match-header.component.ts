8c3c8bb3afbad5ee5530e740910851894dda747f
Wesley <wesleyalkemade@outlook.com>
2026-09-10 14:35:54 +0200

    feat: improve match header with first ban/picks

--- src/app/modules/irc/components/irc-match-header/irc-match-header.component.ts
+++ src/app/modules/irc/components/irc-match-header/irc-match-header.component.ts
@@ -2,7 +2,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Lobby } from '../../../../models/lobby';
 import { IrcChannel } from '../../../../models/irc/irc-channel';
 import { IrcService } from '../../../../services/irc.service';
-import { combineLatest, map } from 'rxjs';
+import { BehaviorSubject, combineLatest, map } from 'rxjs';
 
 @Component({
 	selector: 'app-irc-match-header',
@@ -10,28 +10,97 @@ import { combineLatest, map } from 'rxjs';
 	styleUrl: './irc-match-header.component.scss'
 })
 export class IrcMatchHeaderComponent {
-	@Input() selectedLobby: Lobby;
+	@Input()
+	set selectedLobby(value: Lobby) {
+		this.selectedLobby$.next(value);
+	}
 	@Input() selectedChannel: IrcChannel;
-
 	@Output() adjustScoreEmitter = new EventEmitter<{ team: number, mouseClick: string }>();
 
+	selectedLobby$ = new BehaviorSubject<Lobby>(null);
+
 	matchStatus$ = combineLatest([
+		this.selectedLobby$,
 		this.ircService.nextPick$,
 		this.ircService.matchPoint$,
 		this.ircService.tiebreaker$,
 		this.ircService.hasWon$,
 		this.ircService.teamOneScore$,
-		this.ircService.teamTwoScore$
+		this.ircService.teamTwoScore$,
+		this.ircService.teamOneBans$,
+		this.ircService.teamTwoBans$,
+		this.ircService.teamOneProtects$,
+		this.ircService.teamTwoProtects$
 	])
 		.pipe(
-			map(([nextPick, matchPoint, tiebreaker, hasWon, teamOneScore, teamTwoScore]) => ({
-				nextPick,
-				matchPoint,
-				tiebreaker,
-				hasWon,
-				teamOneScore,
-				teamTwoScore
-			}))
+			map(([
+				selectedLobby, nextPick, matchPoint, tiebreaker, hasWon,
+				teamOneScore, teamTwoScore,
+				teamOneBans, teamTwoBans,
+				teamOneProtects, teamTwoProtects
+			]) => {
+				const matchStatus = {
+					nextPick,
+					matchPoint,
+					tiebreaker,
+					hasWon,
+					teamOneScore,
+					teamTwoScore,
+					teamOneBans,
+					teamTwoBans,
+					teamOneProtects,
+					teamTwoProtects
+				};
+
+				const protectCount = (teamOneProtects?.length || 0) + (teamTwoProtects?.length || 0);
+				const banCount = (teamOneBans?.length || 0) + (teamTwoBans?.length || 0);
+
+				let currentAction = {
+					team: null,
+					action: null
+				};
+
+				if (selectedLobby.tournament) {
+					const totalBans = selectedLobby.selectedStage.bans;
+
+					// TODO: change this once protects are implemented for stages
+					const totalProtects = 2; /* selectedLobby.selectedStage.protects; */
+
+					if (selectedLobby.tournament.protects == true) {
+						// TODO: same as above, implement protects/bans/picks once it has been implemented for stages
+						// if (protectCount < totalProtects) {
+						// 	// currentAction = {
+						// 	// 	team: protectCount === 0 ? selectedLobby.first
+						// 	// }
+						// }
+					}
+					else {
+						if (banCount < totalBans) {
+							currentAction = {
+								team: banCount === 0 ? selectedLobby.firstBan : this.getOtherTeam(selectedLobby, selectedLobby.firstBan),
+								action: 'bans'
+							}
+						}
+						else {
+							currentAction = {
+								team: nextPick,
+								action: 'picks'
+							}
+						}
+					}
+				}
+				else {
+					currentAction = {
+						team: nextPick,
+						action: 'picks'
+					}
+				}
+
+				return {
+					...matchStatus,
+					currentAction
+				};
+			})
 		);
 
 	constructor(
@@ -48,5 +117,9 @@ export class IrcMatchHeaderComponent {
 		else if (event.button == 2) {
 			this.adjustScoreEmitter.emit({ team, mouseClick: 'right' });
 		}
+	}
+
+	getOtherTeam(lobby: Lobby, teamName: string) {
+		return lobby.teamOneName == teamName ? lobby.teamTwoName : lobby.teamOneName;
 	}
 }
